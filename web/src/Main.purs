1986a90fca834a8be93202d13521c62001e6f4ec
Adrian Sieber <mail@adriansieber.com>
2018-11-15 21:46:46 +0000

    Add first draft of web frontend

---
+++ web/src/Main.purs
@@ -0,0 +1,43 @@
+module Main where
+
+import Prelude hiding (div)
+
+import Effect (Effect)
+import Pux (EffModel, start)
+import Pux.DOM.Events (onClick)
+import Pux.DOM.HTML (HTML)
+import Pux.Renderer.React (renderToDOM)
+import Text.Smolder.HTML (button, div, span)
+import Text.Smolder.Markup (text, (#!))
+
+data Event = Increment | Decrement
+
+type State = Int
+
+
+-- | Return a new state (and effects) from each event
+foldp :: Event -> State -> EffModel State Event
+foldp Increment n = { state: n + 1, effects: [] }
+foldp Decrement n = { state: n - 1, effects: [] }
+
+
+-- | Return markup from the state
+view :: State -> HTML Event
+view count =
+  div do
+    button #! onClick (const Increment) $ text "Increment"
+    span $ text (show count)
+    button #! onClick (const Decrement) $ text "Decrement"
+
+
+-- | Start and render the app
+main :: Effect Unit
+main = do
+  app <- start
+    { initialState: 0
+    , view
+    , foldp
+    , inputs: []
+    }
+
+  renderToDOM "#app" app.markup app.input
